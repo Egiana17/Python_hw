f6fbb1c6c92405f165abd7b8b1882c0e3f237d1d
Egiana17 <egishka81@mail.ru>
2025-11-19 21:29:35 +0300

    lesson5

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/dynamicid")
+
+search_input = driver.find_element(By.XPATH, '/html/body/section/div/button')
+search_input.click()
---
+++ 05_lesson/lesson05_task4.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("http://the-internet.herokuapp.com/login")
+search_field = "#username"
+search_input_username_username = driver.find_element(By.XPATH, '//*[@id="username"]')
+search_input_username_username.send_keys("tomsmith")
+search_input_password = driver.find_element(By.XPATH, '//*[@id="password"]')
+search_input_password.send_keys("SuperSecretPassword!")
+search_input = driver.find_element(By.XPATH, '//*[@id="login"]/button')
+search_input.click()
+
+element = driver.find_element_by_id("flash")
+print(element.text)
+driver.quit()
+sleep(10)
