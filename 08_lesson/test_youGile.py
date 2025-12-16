f33591b89f055af5f3613fe9bf53c590d302e8f0
Egiana17 <egishka81@mail.ru>
2025-12-16 18:52:38 +0300

    08_lesson-3

--- 08_lesson/test_youGile.py
+++ 08_lesson/test_youGile.py
@@ -1,18 +1,3 @@
-import requests 
-
-
-base_url = "https://ru.yougile.com"
-key = "xBEFU-KbRU8CihpCPgOjdcu28rBPbSShV1ye-IL4uqMXp6B6nkUP7ur1nj8KBIe4"
-
-
-def test_kreat_progect_positive():
-    headers = {"Authorization": f"Bearer {key}"}
-    body = {"title": "Учебный проект"}
-    response = requests.post(f"{base_url}/api-v2/projects", headers=headers, json=body)
-    assert response.status_code == 201
-    response_json = response.json()
-    assert response_json['title'] == 'Учебный проект'
-
 def test_kreat_progect_negative():
     headers = {"Authorization": "Bearer "}
     body = {"title": "Учебный проект"}
